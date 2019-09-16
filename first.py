c5e888346d28fd5725744d6d18a11a362c76da38
Noah Wehner <54412600+NoahWehner@users.noreply.github.com>
2019-09-16 11:51:53 -0400

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+#Noah Wehner
+#A Program for stocks
+name = input("whats your name?")
+age = input("whats your age?")
+print("welcome", name,"You are", age, "years old")
+print("welcome to the stock market, time to sit back and relax but first...")
+answer = input("what is 2 + 2 = ")
+if ( answer == "4"):
+    print("Welcome to the future")
+else:
+    print("That's the wrong numberrrrr")
