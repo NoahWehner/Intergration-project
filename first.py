a377525d61ce660e1cb3f83c671a2638b7a569dd
Noah Wehner <54412600+NoahWehner@users.noreply.github.com>
2019-11-18 14:45:40 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1,11 +1,41 @@
 #Noah Wehner
-#A Program for stocks
+#integration project
 name = input("whats your name?")
 age = input("whats your age?")
 print("welcome", name,"You are", age, "years old")
-print("welcome to the stock market, time to sit back and relax but first...")
+#greeting code
+print("welcome to a simple integration project but answer this simple question ")
 answer = input("what is 2 + 2 = ")
+#simple calculations
 if ( answer == "4"):
-    print("Welcome to the future")
+    print("correct!")
 else:
-    print("That's the wrong numberrrrr"
+    print("That's the wrong numberrrrr")
+#standard conditional structure
+mystr = "123"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+mytuple = ("3", "2", "1")
+
+for x in mytuple:
+  print(x)
+#iterative structures
+def my_function(fun):
+  print( " It's cold " + fun)
+
+my_function("Outside")
+my_function("Inside")
+my_function("Everywhere")
+#function
+
+def my_function(colors):
+  for x in colors:
+    print(x)
+
+colors = ["blue", "Old Gold", "White"]
+
+my_function(colors)
+#passing parameters
